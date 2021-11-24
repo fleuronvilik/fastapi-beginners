c949f98a6581b8a55994e99337c977b9d50c644e
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-11-24 15:10:17 +0100

    setup db and use driver to query
    CRUD rewritten
    DBMS: PostgreSQL
    Driver: psycopg2

--- app/main.py
+++ app/main.py
@@ -1,40 +1,38 @@
+import time
+import psycopg2
+
 from fastapi import FastAPI, Response, status, HTTPException
-# from fastapi.params import Body
 from pydantic import BaseModel
+from psycopg2.extras import RealDictCursor
 
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
 
-my_posts = [
-    {
-        "id": 1,
-        "title": "Quod Ducimus Omnis",
-        "content": "Quod Ducimus Omnis"
-    },
-    {
-        "id": 2,
-        "title": "Python API Development",
-        "content": "Watch the full length course on our YT channel"
-    }
-]
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            database='fastapi',
+            user='postgres',
+            password='postgres',
+            cursor_factory=RealDictCursor
+        )
+        cursor = conn.cursor()
+        break
+    except Exception as error:
+        print(error)
+        time.sleep(2)
 
 app = FastAPI()
-
-def get_post_by_id(id):
-    for post_at, post in enumerate(my_posts):
-        if post["id"] == id:
-            return post, post_at
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, # res.status_code = status.HTTP_404_NOT_FOUND
-        detail=f"No post with ID {id}" # return {"message": "No post with this ID"}
-    )
-
-def get_last_id():
-    if not my_posts:
-        return 0
-    return my_posts[-1]["id"]
+ 
+def raise_404_or_not(post, id=None):
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No post with ID {id}"
+        )
 
 @app.get('/')
 def root():
@@ -42,33 +40,36 @@ def root():
 
 @app.get('/posts')
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = get_last_id() + 1
-    my_posts.append(post_dict)
-    return {
-        # "message": "Successfully created post",
-        "data": post_dict
-    }
+    sql_insert = """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *"""
+    cursor.execute(sql_insert, (post.title, post.content, post.published))
+    conn.commit()
+    return {"data": cursor.fetchone()}
 
 @app.get('/posts/{id}')
-def get_post(id: int, res: Response):
-    data, _ = get_post_by_id(id) 
-    return {"data": data}
+def get_post(id: int):
+    cursor.execute("""SELECT * FROM posts WHERE id=%s""", (id,))
+    post = cursor.fetchone()
+    raise_404_or_not(post, id)
+    return {"data": post}
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def del_post(id: int):
-    _, post_at = get_post_by_id(id)
-    my_posts.pop(post_at)
+    cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""", (id,))
+    raise_404_or_not(cursor.fetchone(), id)
+    conn.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put('/posts/{id}')
 def update_post(id: int, post: Post):
-    _, post_at = get_post_by_id(id)
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[post_at] = post_dict
-    return {"data": post_dict}
+    sql_update = """UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *"""
+    cursor.execute(sql_update, (post.title, post.content, post.published, id))
+    post_up = cursor.fetchone()
+    raise_404_or_not(post_up, id)
+    conn.commit()
+    return {"data": post_up}

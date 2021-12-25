00fc900e290e020aeb99d7f5324b7015ad46f240
Fleuron Gnadekpa <vilfleur@gmail.com>
2021-12-26 00:20:01 +0100

    cors: grant access to api from any clients

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .db import engine
 from .routers import post, user, auth, vote
 from . import models
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine) # alembic does the work now
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
-# @app.get('/')
-# def root():
-#     return {"message": "Welcome to my uncomplete API"}
+@app.get('/')
+def root():
+    return {"message": "Welcome to my uncomplete API"}
